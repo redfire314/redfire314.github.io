f72bb62dcd3ce15bc6027d06780705345ff8d613
redfire314 <leandro.araujowm@gmail.com>
2021-10-20 19:49:31 -0300

    One-Page-Sample added
    Made with Materialize

---
+++ one-page-sample/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+    $('.fixed-action-btn').floatingActionButton()
+    $('.sidenav').sidenav()
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    })
+})
