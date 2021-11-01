75f883389fdf153374a7d6ec8e6959a434099776
redfire314 <leandro.araujowm@gmail.com>
2021-11-01 10:54:28 -0300

    Optimization

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-const load = setInterval(() => {
-    if(document.readyState == "complete") {
-        // Hamburger menu event
-        document.querySelector('#hamburger-menu-container').addEventListener('click', () => {
-            document.querySelector('#hamburger-menu').classList.toggle('open')
-            document.querySelector('.nav-menu').classList.toggle('open')
-        })
-
-        clearInterval(load)
-    }
-}, 100)
+document.querySelector('#hamburger-menu-container').addEventListener('click', () => {
+    document.querySelector('#hamburger-menu').classList.toggle('open')
+    document.querySelector('.nav-menu').classList.toggle('open')
+})
