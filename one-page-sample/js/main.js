b3ed431181e19fade430e3c9ac034194f78dc4cf
redfire314 <leandro.araujowm@gmail.com>
2021-11-25 21:15:11 -0300

    One page sample removed

--- one-page-sample/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(() => {
-    $('.fixed-action-btn').floatingActionButton()
-    $('.sidenav').sidenav()
-    $('.carousel.carousel-slider').carousel({
-        fullWidth: true,
-        indicators: true
-    })
-})
