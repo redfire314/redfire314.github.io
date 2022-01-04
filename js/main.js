8d6133eb65451870e439ee0b5b0f0d3349e56cf7
redfire314 <leandro.araujowm@gmail.com>
2022-01-04 15:44:52 -0300

    Image optimization

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
+let loadedImgIndex = []
+
+function showOnLoad() {
+    document.querySelectorAll('img[data-src]').forEach((img, index) => {
+        if (img.getBoundingClientRect().top < window.innerHeight && !loadedImgIndex.includes(index)) {
+            loadedImgIndex.push(index)
+            img.src = img.getAttribute('data-src')
+        }
+    })
+}
+
 document.querySelector('#hamburger-menu-container').addEventListener('click', () => {
     document.querySelector('#hamburger-menu').classList.toggle('open')
     document.querySelector('.nav-menu').classList.toggle('open')
 })
+
+window.addEventListener('scroll', showOnLoad)
